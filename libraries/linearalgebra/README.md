96116530aabb73390f7486023a5efaa4130dab7a
Matheus Miranda <rock.matheus.mh@gmail.com>
2021-10-16 23:16:32 -0300

    Directories were renamed.

--- libraries/linearalgebra/README.md
+++
@@ -1,4 +0,0 @@
-# Linear Algebra Essentials.
-- Vector,
-- Matrix,
-- Coordinates Systems.
--- libraries/linearalgebra/__init__.py
+++
@@ -1,5 +0,0 @@
-from .coordinates import Cartesian
-from .vectors import Vector
-from .intersections import Line
-
-'''Vectors, Intersections, Coordinates Systems & Matrices.'''
